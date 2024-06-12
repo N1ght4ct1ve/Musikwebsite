e3d74863d4128754db84695d41a19f4622e060f9
pheineke <heineke.pe@gmail.com>
2024-06-13 01:57:54 +0200

    .

---
+++ static/js/sidebar.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("root").style.marginLeft = "250px";
+  }
+  
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("root").style.marginLeft = "0";
+} 
