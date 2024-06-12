ac6bc7ecb22ec1dc343a7c849882bc62798c325a
pheineke <heineke.pe@gmail.com>
2024-06-13 00:47:56 +0200

    transitions

--- static/js/index.js
+++ static/js/index.js
@@ -23,19 +23,28 @@ var theme = "light";
 
 const toggle = document.getElementById("toggleDark");
 const body = document.querySelector("body");
+const main = document.querySelector("main");
 
 toggle.addEventListener("click", function(){
     this.classList.toggle("bi-moon");
     if(this.classList.toggle("bi-brightness-high-fill")){
 
         body.style.background = "#D7DFE2";
-        body.style.color ="black";
+        body.style.color = "black";
         body.style.transition = "2s";
+
+        main.style.background = "#b5b5b5";
+        main.style.color = "white";
+        main.style.transition = "2s";
 
     }else{
         body.style.background ="#030303" ;
         body.style.color = "white";
         body.style.transition = "2s";
+
+        main.style.background = "#3c3c3c";
+        main.style.color = "black";
+        main.style.transition = "2s"
 
     };
 });
