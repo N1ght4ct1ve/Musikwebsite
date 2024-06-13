e78f90bb0a6eafdf67bc6d9eaae1a1589b7d0f9f
pheineke <heineke.pe@gmail.com>
2024-06-13 02:03:15 +0200

    animationen fix ig

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,10 +1,16 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("root").style.marginLeft = "250px";
+    var sidebar = document.getElementById("sidebar")
+    var root = document.getElementById("root")
+    sidebar.style.width = "250px";
+    root.style.marginLeft = "250px";
+    sidebar.style.transition = "2s";
   }
   
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("root").style.marginLeft = "0";
+    var sidebar = document.getElementById("sidebar")
+    var root = document.getElementById("root")
+    sidebar.style.width = "0";
+    root.style.marginLeft = "0";
+    sidebar.style.transition = "2s";
 } 
