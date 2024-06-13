a4d6f72879cdeaf333baa0fc605d9a472ee6a689
pheineke <heineke.pe@gmail.com>
2024-06-13 02:10:53 +0200

    .

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,16 +1,15 @@
-function openNav() {
-    var sidebar = document.getElementById("sidebar")
-    var root = document.getElementById("root")
-    sidebar.style.width = "250px";
-    root.style.marginLeft = "250px";
-    sidebar.style.transition = "2s";
-  }
-  
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    var sidebar = document.getElementById("sidebar")
-    var root = document.getElementById("root")
-    sidebar.style.width = "0";
-    root.style.marginLeft = "0";
-    sidebar.style.transition = "2s";
-} 
+function toggleNav() {
+    var sidebar = document.getElementById("sidebar");
+    var root = document.getElementById("root");
+    var button = document.querySelector(".openbtn");
+
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        root.style.marginLeft = "0";
+        button.innerHTML = "&#9776;"; // Hamburger icon
+    } else {
+        sidebar.style.width = "250px";
+        root.style.marginLeft = "250px";
+        button.innerHTML = "&times;"; // Close icon
+    }
+}
